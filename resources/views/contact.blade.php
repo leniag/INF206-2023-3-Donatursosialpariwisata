0638084fc967a9034f305ca1e5cf1ef9328dd9e7
ulan sawalia <ulan21@mhs.unsyiah.ac.id>
2023-05-11 20:33:55 +0700

    feat : add folder views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,52 @@
+<section class="section" id="contact">
+
+<div class="container">
+            <div class="row">
+                <div class="col-lg-6 align-self-center">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h1>Contact Us</h1>
+                            <h2>Jika Anda memiliki pertanyaan atau saran mengenai Donatur Pariwisata, silakan hubungi kami melalui informasi kontak di bawah ini: </h2>
+                        </div>
+                        <p>
+Alamat Kantor:
+Jl. Pariwisata No. 123, Jakarta Selatan
+Indonesia
+<br> <br>
+
+Email:
+info@donaturpariwisata.co.id
+<br> <br>
+Telepon:
++62 812-3456-7890
+<br> <br>
+Jam Operasional:
+Senin - Jumat, 08.00 - 17.00 WIB
+<br> <br>
+Anda juga dapat menghubungi kami melalui media sosial:
+<br> <br>
+Instagram: @donaturpariwisata
+<br>
+Facebook: Donatur Pariwisata
+<br>
+Twitter: @donaturpariwisata
+<br>
+Kami akan berusaha untuk merespon pertanyaan dan saran Anda secepat mungkin. Terima kasih telah menghubungi Donatur Pariwisata! 
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="phone">
+                                    <i class="fa fa-phone"></i>
+                                    <h4>Nomor Telepon</h4>
+                                    <span><a href="#">080-090-0990</a><br><a href="#">080-090-0880</a></span>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="message">
+                                    <i class="fa fa-envelope"></i>
+                                    <h4>Email</h4>
+                                    <span><a href="#">DSP@company.com</a><br><a href="#">infoDSP@company.com</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
